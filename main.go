3591032a387d537080f0410aae5ff13132241d91
sagar.dobariya <sagar.dobariya@authbridge.com>
2022-09-25 18:55:14 +0530

    change port reading

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"signaling-server/configs"
+	"os"
 	"signaling-server/controllers"
 	"signaling-server/helpers"
 )
@@ -16,7 +16,11 @@ func main() {
 	controllers.AllRoom.New()
 	http.HandleFunc("/room", controllers.CreateRoom)
 	http.HandleFunc("/join", controllers.JoinRoom)
-	appPort := configs.GetEnvWithKey(configs.KEY_APP_PORT, "8080")
+	// appPort := configs.GetEnvWithKey(configs.KEY_APP_PORT, "8080")
+	appPort := os.Getenv("PORT")
+	if appPort == "" {
+		appPort = "8080"
+	}
 	severAddres := fmt.Sprintf(":%s", appPort)
 	fmt.Println("port:", severAddres)
 	err := http.ListenAndServe(severAddres, nil)
